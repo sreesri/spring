0eb869a6574732df0109ee7ad1ded4edab0b33c3
mm0415 <mm0415@MMDT0181.maven.local>
2018-06-21 15:38:57 +0530

    New File

---
+++ spring_demo1/src/com/sree/demo/SetterDemoApp.java
@@ -0,0 +1,17 @@
+package com.sree.demo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		CricketCoach coach = context.getBean("myCricketCoach",CricketCoach.class);
+		System.out.println(coach.getDailyWorkout());
+		System.out.println(coach.getDailyFortune());
+		System.out.println(coach.getEmail());
+		System.out.println(coach.getTeam());
+		context.close();
+	}
+
+}
