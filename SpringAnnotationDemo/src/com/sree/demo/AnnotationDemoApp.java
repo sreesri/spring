6d44df3b51e79212353dca8c21d39e968a9b26f9
mm0415 <mm0415@MMDT0181.maven.local>
2018-06-22 12:02:22 +0530

    testing

--- SpringAnnotationDemo/src/com/sree/demo/AnnotationDemoApp.java
+++ SpringAnnotationDemo/src/com/sree/demo/AnnotationDemoApp.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class AnnotationDemoApp {
 
 	public static void main(String[] args) {
+		System.out.println("statred");
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Coach coach = context.getBean("tennisCoach", Coach.class);
 		System.out.println(coach.getDailyWorkout());
